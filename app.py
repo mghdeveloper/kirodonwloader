643e5c606182c64647255dc8e21682eebd7797c7
mghdeveloper <140075630+mghdeveloper@users.noreply.github.com>
2026-03-07 22:51:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import os
 import uuid
+import threading
 from flask import Flask, request, render_template, jsonify, send_file
+
 from downloader import convert_m3u8
 
 app = Flask(__name__)
 
-DOWNLOAD_DIR = "downloads"
+DOWNLOAD_DIR = os.path.abspath("downloads")
 os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 
 tasks = {}
+
 
 @app.route("/")
 def home():
@@ -18,21 +21,29 @@ def home():
 @app.route("/convert", methods=["POST"])
 def convert():
 
-    m3u8_url = request.json["url"]
+    data = request.get_json()
+
+    m3u8_url = data["url"]
 
     task_id = str(uuid.uuid4())
-    output = f"{DOWNLOAD_DIR}/{task_id}.mp4"
+
+    output_file = os.path.join(DOWNLOAD_DIR, f"{task_id}.mp4")
 
     tasks[task_id] = {
         "progress": 0,
         "status": "downloading",
-        "file": output
+        "file": output_file,
+        "error": None
     }
 
     def run():
-        convert_m3u8(m3u8_url, output, tasks, task_id)
+        try:
+            convert_m3u8(m3u8_url, output_file, tasks, task_id)
+        except Exception as e:
+            print("FATAL ERROR:", e)
+            tasks[task_id]["status"] = "error"
+            tasks[task_id]["error"] = str(e)
 
-    import threading
     threading.Thread(target=run).start()
 
     return jsonify({"task_id": task_id})
@@ -40,13 +51,23 @@ def convert():
 
 @app.route("/progress/<task_id>")
 def progress(task_id):
-    return jsonify(tasks.get(task_id, {}))
+
+    if task_id not in tasks:
+        return jsonify({"error": "task not found"}), 404
+
+    return jsonify(tasks[task_id])
 
 
 @app.route("/download/<task_id>")
 def download(task_id):
 
+    if task_id not in tasks:
+        return {"error": "invalid task"}, 404
+
     file = tasks[task_id]["file"]
+
+    if not os.path.exists(file):
+        return {"error": "file not ready"}, 404
 
     return send_file(file, as_attachment=True)
 
